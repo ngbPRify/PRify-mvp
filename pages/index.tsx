a18197365fb2e48e2fa32a79840fb520156b92f3
ngbPRify <ngbbuisness@gmail.com>
2025-05-29 21:45:31 +0200

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: 'sans-serif' }}>
+      <h1>Welcome to PRify MVP</h1>
+      <p>Se vedi questa pagina, l’app sta girando correttamente!</p>
+    </div>
+  );
+}
